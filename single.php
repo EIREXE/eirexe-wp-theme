8ce72f81171bc7279cff7025fca12f09fcfd454a
Alex Roman Nuñez <eirexe123@gmail.com>
2016-09-05 23:58:51 +0200

    Arreglar un <? corto, PHP es caca.

--- single.php
+++ single.php
@@ -20,43 +20,43 @@
 
 <div class="container main">
 
-    
+
      </div><!--/.row -->
 
      <div class="row main-content <?php if( get_theme_mod( 'blog_sidebar' ) == 'right') : ?>
-	 rightsid <?php else: ?> leftsid<? endif; ?>">	 
-	 
-	 	 	 
+	 rightsid <?php else: ?> leftsid<?php endif; ?>">
+
+
 	 <?php if( get_theme_mod( 'blog_sidebar' ) == 'left') : ?>
-	 
-         <div class="col-lg-9 col-md-9 col-sm-9 col-9 aparece">     
-         <?php get_template_part('singlemain'); ?>                
-         </div> <!-- /.col -->      				
-        <div class="col-lg-3 col-md-3 col-sm-3 col-3 sidebar-wrapper">           
+
+         <div class="col-lg-9 col-md-9 col-sm-9 col-9 aparece">
+         <?php get_template_part('singlemain'); ?>
+         </div> <!-- /.col -->
+        <div class="col-lg-3 col-md-3 col-sm-3 col-3 sidebar-wrapper">
         <?php get_sidebar('post'); ?>
         </div><!--/.col -->
-		<div class="col-lg-9 col-md-9 col-sm-9 col-9 desaparece">     
-        <?php get_template_part('singlemain'); ?>              
-        </div> <!-- /.col -->    
-		
+		<div class="col-lg-9 col-md-9 col-sm-9 col-9 desaparece">
+        <?php get_template_part('singlemain'); ?>
+        </div> <!-- /.col -->
+
 	 <?php else: ?>
 
-     	  <div class="col-lg-9 col-md-9 col-sm-9 col-9 ">     
-         <?php get_template_part('singlemain'); ?>                
-         </div> <!-- /.col -->      				
-        <div class="col-lg-3 col-md-3 col-sm-3 col-3 sidebar-wrapper">           
+     	  <div class="col-lg-9 col-md-9 col-sm-9 col-9 ">
+         <?php get_template_part('singlemain'); ?>
+         </div> <!-- /.col -->
+        <div class="col-lg-3 col-md-3 col-sm-3 col-3 sidebar-wrapper">
         <?php get_sidebar('post'); ?>
         </div><!--/.col -->
-		
+
 	 <?php endif; ?>
-	 
+
 
      </div> <!--/.row main-content -->
 
 </div><!-- container -->
-<?php if( get_theme_mod( 'blog_sidebar' ) == 'right') : 
- get_footer(); 
+<?php if( get_theme_mod( 'blog_sidebar' ) == 'right') :
+ get_footer();
 else:
-get_footer('left-sidebar'); 
-endif; 
+get_footer('left-sidebar');
+endif;
 ?>
